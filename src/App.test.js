69a5f291f2e1151e3dd677e14c1afa060e889f4f
NyanShen <sibaoemail@126.com>
2019-08-03 16:52:59 +0800

    delete unuse file add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {shallow} from "enzyme";
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // const container = div.getElementsByClassName('App');
+  // expect(container.length).toBe(1);
+  // ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('[data-test="container"]').length).toBe(1);
+  //console.log(wrapper.debug())
+  expect(wrapper.find('[data-test="container"]').prop('title')).toBe('test');
+  expect(wrapper.find('[data-test="container"]')).toExist();
 });
