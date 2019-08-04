7c23800dc761b5181d4ae76e7c5cc6144085acb3
NyanShen <sibaoemail@126.com>
2019-08-04 17:15:28 +0800

    style optimize

--- src/components/UndoList.jsx
+++ src/components/UndoList.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import "./style.css";
 
 export default class UndoList extends React.Component {
 
     render() {
         const { list, deleteItem } = this.props;
         return (
-            <div>
-                <div data-test="count">{list.length}</div>
-                <ul>
+            <div className="undo-list">
+                <div className="undo-list-title">
+                    正在进行
+                    <span className="undo-list-count" data-test="count">{list.length}</span>
+                </div>
+
+                <ul className="undo-list-content">
                     {
                         list.map((item, index) => {
                             return (
-                                <li data-test="listItem" key={`${item}-${index}`}>
+                                <li 
+                                className="undo-list-item"
+                                data-test="listItem" 
+                                key={`${item}-${index}`}
+                                >
                                     {item}
-                                    <span data-test="deleteItem" onClick={() => {deleteItem(index)}}>-</span>
-                                </li> 
+                                    <span 
+                                    className="undo-list-delete"
+                                    data-test="deleteItem" 
+                                    onClick={() => { deleteItem(index) }}>-</span>
+                                </li>
                             )
                         })
                     }
