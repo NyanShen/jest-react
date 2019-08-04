67f62a05aaee3be40e0ac05e4b9742eaa2d5142d
NyanShen <sibaoemail@126.com>
2019-08-04 17:30:19 +0800

    optimize test file

--- src/components/__tests__/unit/Header.jsx
+++ src/components/__tests__/unit/Header.jsx
@@ -1,78 +1,81 @@
 import React from "react";
-import {shallow} from "enzyme";
+import { shallow } from "enzyme";
 import Header from "../../Header";
-import {findTestWrapper} from "../../../common/util/testUtils"
+import { findTestWrapper } from "../../../common/util/testUtils"
 
-it("Header 样式渲染正常", () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper).toMatchSnapshot();
-})
-
-it("Header 组件包含一个 input 输入框", () => {
-    const wrapper = shallow(<Header />);
-    const inputElem = findTestWrapper(wrapper, "input");
-    expect(inputElem.length).toBe(1);
-})
-
-it("Header input 输入框, 初始化应该为空", () => {
-    const wrapper = shallow(<Header />);
-    const inputElem = wrapper.find("[data-test='input']");
-    expect(inputElem.prop("value")).toEqual("");
-})
-
-it("Header input 输入框, 当输入时会发生改变", () => {
-    const wrapper = shallow(<Header />);
-    const inputElem = wrapper.find("[data-test='input']");
-    /**
-     * 模拟change事件，输入test nyan
-     */
-    inputElem.simulate("change", {
-        target: {
-            value: "test nyan"
-        }
+describe("Header Componet Test", () => {
+    it("样式渲染正常", () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper).toMatchSnapshot();
     })
-    expect(wrapper.state("value")).toEqual("test nyan");
-})
 
-it("Header input 输入框，回车时，将没有内容，也没有操作", () => {
-    const fn = jest.fn();
-    const wrapper = shallow(<Header addUndoItem={fn}/>);
-    const inputElem = wrapper.find("[data-test='input']");
-    wrapper.setState({value: ""})
-    inputElem.simulate("kepUp", {
-        keyCode: 13
+    it("组件包含输入框", () => {
+        const wrapper = shallow(<Header />);
+        const inputElem = findTestWrapper(wrapper, "input");
+        expect(inputElem.length).toBe(1);
     })
-    expect(fn).not.toHaveBeenCalled();
-})
 
-it("Header input 输入框，回车时，有内容，函数应该被调用", () => {
-    const fn = jest.fn();
-    const wrapper = shallow(<Header addUndoItem={fn}/>);
-    const inputElem = wrapper.find("[data-test='input']");
-    // 准备数据
-    wrapper.setState({value: "jest react"})
-    /**
-     * 设置keyUp的keyCode模拟回车
-     */
-    inputElem.simulate("keyUp", {
-        keyCode: 13
+    it("输入框内容初始化应该为空", () => {
+        const wrapper = shallow(<Header />);
+        const inputElem = wrapper.find("[data-test='input']");
+        expect(inputElem.prop("value")).toEqual("");
     })
-    expect(fn).toHaveBeenCalled();
-    expect(fn).toHaveBeenLastCalledWith("jest react"); // 最后的参数
-})
 
-it("Header input 输入框，回车时，有内容，内容应该被清除", () => {
-    const fn = jest.fn();
-    const wrapper = shallow(<Header addUndoItem={fn}/>);
-    const inputElem = wrapper.find("[data-test='input']");
-    // 准备数据
-    wrapper.setState({value: "jest react add one"})
-    /**
-     * 设置keyUp的keyCode模拟回车
-     */
-    inputElem.simulate("keyUp", {
-        keyCode: 13
+
+    it("输入框随用户输入时发生改变", () => {
+        const wrapper = shallow(<Header />);
+        const inputElem = wrapper.find("[data-test='input']");
+        /**
+         * 模拟change事件，输入test nyan
+         */
+        inputElem.simulate("change", {
+            target: {
+                value: "test nyan"
+            }
+        })
+        expect(wrapper.state("value")).toEqual("test nyan");
     })
-    const newInputElem = wrapper.find("[data-test='input']");
-    expect(newInputElem.prop("value")).toBe("");
+
+    it("输入框没有内容时，回车时无反应", () => {
+        const fn = jest.fn();
+        const wrapper = shallow(<Header addUndoItem={fn} />);
+        const inputElem = wrapper.find("[data-test='input']");
+        wrapper.setState({ value: "" })
+        inputElem.simulate("kepUp", {
+            keyCode: 13
+        })
+        expect(fn).not.toHaveBeenCalled();
+    })
+
+    it("输入框有内容触发回车时，函数应该被调用", () => {
+        const fn = jest.fn();
+        const wrapper = shallow(<Header addUndoItem={fn} />);
+        const inputElem = wrapper.find("[data-test='input']");
+        // 准备数据
+        wrapper.setState({ value: "jest react" })
+        /**
+         * 设置keyUp的keyCode模拟回车
+         */
+        inputElem.simulate("keyUp", {
+            keyCode: 13
+        })
+        expect(fn).toHaveBeenCalled();
+        expect(fn).toHaveBeenLastCalledWith("jest react"); // 最后的参数
+    })
+
+    it("输入框有内容触发回车时，内容应该被清除", () => {
+        const fn = jest.fn();
+        const wrapper = shallow(<Header addUndoItem={fn} />);
+        const inputElem = wrapper.find("[data-test='input']");
+        // 准备数据
+        wrapper.setState({ value: "jest react add one" })
+        /**
+         * 设置keyUp的keyCode模拟回车
+         */
+        inputElem.simulate("keyUp", {
+            keyCode: 13
+        })
+        const newInputElem = wrapper.find("[data-test='input']");
+        expect(newInputElem.prop("value")).toBe("");
+    })
 })
--- src/components/__tests__/unit/TodoList.jsx
+++ src/components/__tests__/unit/TodoList.jsx
@@ -1,44 +1,47 @@
 import React from "react";
-import {shallow} from "enzyme";
+import { shallow } from "enzyme";
 import TodoList from "../../../containers/TodoList";
 
-it("TodoList 初始化列表为空", () => {
-    const wrapper = shallow(<TodoList />);
-    expect(wrapper.state("undoList")).toEqual([]);
-})
+describe("TodoList Comonent Test", () => {
 
-it("TodoList Header 应该有一个增加undoList的方法", () => {
-    const wrapper = shallow(<TodoList />);
-    const header = wrapper.find("Header");
-    //expect(header.prop("addUndoItem")).toBe(wrapper.instance().addUndoItem);
-    expect(header.prop("addUndoItem")).toBeTruthy();
-})
+    it("初始化列表为空", () => {
+        const wrapper = shallow(<TodoList />);
+        expect(wrapper.state("undoList")).toEqual([]);
+    })
 
-it("TodoList Header 调用回车时，即addUndoItem执行时，应该新增内容", () => {
-    const wrapper = shallow(<TodoList />);
-    /**
-     * const header = wrapper.find("Header");
-     * const addFn = header.prop("addUndoItem");
-     * addFn("add item one");
-     * 这样操作，测试就会跟Header组件耦合（像集成测试），应该修改为一下方案
-     */
-    wrapper.instance().addUndoItem("add item one")
-    expect(wrapper.state("undoList").length).toBe(1);
-    expect(wrapper.state("undoList")[0]).toBe("add item one")
-})
+    it("Header组件存在addUndoItem的属性", () => {
+        const wrapper = shallow(<TodoList />);
+        const header = wrapper.find("Header");
+        //expect(header.prop("addUndoItem")).toBe(wrapper.instance().addUndoItem);
+        expect(header.prop("addUndoItem")).toBeTruthy();
+    })
 
-it("TodoList UndoList 应该有一个list和deleteItem两个属性", () => {
-    const wrapper = shallow(<TodoList />);
-    const undoList = wrapper.find("UndoList");
-    expect(undoList.prop("list")).toBeTruthy();
-    expect(undoList.prop("deleteItem")).toBeTruthy();
-})
+    it("addUndoItem方法被调用时，undoList数据项新增", () => {
+        const wrapper = shallow(<TodoList />);
+        /**
+         * const header = wrapper.find("Header");
+         * const addFn = header.prop("addUndoItem");
+         * addFn("add item one");
+         * 这样操作，测试就会跟Header组件耦合（像集成测试），应该修改为一下方案
+         */
+        wrapper.instance().addUndoItem("add item one")
+        expect(wrapper.state("undoList").length).toBe(1);
+        expect(wrapper.state("undoList")[0]).toBe("add item one")
+    })
 
-it("TodoList 当deleteItem执行时，应该删除对应内容", () => {
-    const wrapper = shallow(<TodoList />);
-    wrapper.setState({
-        undoList: ["react", "jest", "enzyme"]
-    });
-    wrapper.instance().deleteItem(1);
-    expect(wrapper.state("undoList")).toEqual(["react", "enzyme"])
+    it("UndoList 组件应该有接收list和deleteItem两个属性", () => {
+        const wrapper = shallow(<TodoList />);
+        const undoList = wrapper.find("UndoList");
+        expect(undoList.prop("list")).toBeTruthy();
+        expect(undoList.prop("deleteItem")).toBeTruthy();
+    })
+
+    it("deleteItem被执行时，应该删除对应数据项", () => {
+        const wrapper = shallow(<TodoList />);
+        wrapper.setState({
+            undoList: ["react", "jest", "enzyme"]
+        });
+        wrapper.instance().deleteItem(1);
+        expect(wrapper.state("undoList")).toEqual(["react", "enzyme"])
+    })
 })
